37b469a7faa3f346d070fc04d340ffe13bf0ad94
brmat <baur.mathieu@laposte.net>
2017-01-31 16:09:37 +0100

    indexation

---
+++ 02-PointJardin/header.php
@@ -0,0 +1,18 @@
+<head>
+
+	<!--Let browser know website is optimized for mobile-->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+	<!--Import Google Icon Font-->
+	<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/materialize.css">
+	<link type="text/css" rel="stylesheet" href="css/materialize.min.css">
+	<script src="js/jquery.min.js"></script>
+ 
+	<script>$(".dropdown-button").dropdown();</script>
+
+	<title>Point Jardin matériaux pour le jardin Saint Rémy sur Avre</title>
+
+</head>
---
+++ 02-PointJardin/index.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html>
+<head>
+
+	<!--Let browser know website is optimized for mobile-->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+	<!--Import Google Icon Font-->
+	<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/materialize.css">
+	<link type="text/css" rel="stylesheet" href="css/materialize.min.css">
+	<script src="js/jquery.min.js"></script>
+ 
+	<script>$(".dropdown-button").dropdown();</script><!-- Scrit dropdown-->
+
+	<title>Point Jardin matériaux pour le jardin Saint Rémy sur Avre</title><!-- Title-->
+
+</head>
+<body>
+
+
+
+<nav><!-- Navbar-->
+    <div class="nav-wrapper">
+      <form>
+        <div class="input-field">
+          <input id="search" type="search" required>
+          <label for="search"><i class="material-icons">search</i></label>
+          <i class="material-icons">close</i>
+        </div>
+      </form>
+    </div>
+
+	<ul id="dropdown2" class="dropdown-content">
+	  <li><a href="#!">Graviers décoratifs</a></li>
+	  <li><a href="#!"><a href="#!">Galets</a></li>
+	  <li><a href="#!">Ardoise</a></li>
+	  <li><a href="#!">Garden Stone</a></li>
+	  <li><a href="#!">La brique</a></li>
+	  <li><a href="#!">Pouzzolane</a></li>
+	  <li><a href="#!">Gros volumes</a></li>
+	  <li><a href="#!">Le schiste</a></li>
+	  <li><a href="#!">Le sable</a></li>
+	  <li><a href="#!">Opus incertum</a></li>
+	</ul>
+
+	<ul id="dropdown3" class="dropdown-content">
+	  <li><a href="#!">Écorce de pin</a></li>
+	  <li><a href="#!">Plaquette peuplier</a></li>
+	  <li><a href="#!">Terreau</a></li>
+	  <li><a href="#!">Terre végétale</a></li>
+	  <li><a href="#!">Terre de bruyère</a></li>
+	  <li><a href="#!">Chanvre</a></li>
+	   <li><a href="#!">Noyaux de fruits</a></li>
+	</ul>
+
+	<ul id="dropdown4" class="dropdown-content">
+	  <li><a href="#!">Bordures</a></li>
+	  <li><a href="#!">Engrais</a></li>
+	  <li><a href="#!">Gazon de placage</a></li>
+	  <li><a href="#!">Traverse</a></li>
+	  <li><a href="#!">Gazon</a></li>
+	  <li><a href="#!">Dalles alvéolées</a></li>
+	</ul>
+
+	<ul id="dropdown5" class="dropdown-content">
+	  <li><a href="#!">Livraison</a></li>
+	  <li><a href="#!">Vente en vrac</a></li>
+	  <li><a href="#!">Conseil de pro</a></li>
+	  <li><a href="#!">Devis gratuit</a></li>
+	  <li><a href="#!">Recherche spécifique</a></li>
+	  <li><a href="#!">Pré-commande</a></li>
+	  <li><a href="#!">Compte client</a></li>
+	</ul>
+
+
+	<nav class="nav2">
+	 <div class="dropdown-button"><a href="contact.html"><i class="mdi-action-perm-contact-cal left"></i>CONTACT</a>
+	  <ul class="right hide-on-med-and-down">
+		<!-- Dropdown Trigger -->
+		
+		<li><a class="dropdown-button" href="index.php">ACCUIEL</a></li>
+		<li><a class="dropdown-button" href="#!" data-activates="dropdown2">MINERAL<i class="material-icons right">arrow_drop_down</i></a></li>
+		<li><a class="dropdown-button" href="#!" data-activates="dropdown3">VEGETAL<i class="material-icons right">arrow_drop_down</i></a></li>
+		<li><a class="dropdown-button" href="#!" data-activates="dropdown4">ACCESSOIRES<i class="material-icons right">arrow_drop_down</i></a></li>
+		<li><a class="dropdown-button" href="#!" data-activates="dropdown5">NOS SERVICES<i class="material-icons right">arrow_drop_down</i></a></li>
+		
+	  </ul>
+	 </div>
+	</nav>
+	
+</nav><!--/Navbar-->
+
+
+<div class="video-wrapper">
+	<div class="video-container">
+		<iframe src="https://www.youtube.com/embed/szGkJgvt2-0?rel=0&amp;loop=1&amp" frameborder="0" allowfullscreen></iframe>
+    </div>
+</div><!--/Video-wrapper -->
+
+<div class="section2">
+	<div class="row2">
+		<img src="imgs/logo2.png" class="logo">
+	</div>  
+
+	<div class="row3">
+		<h2>BIENVENUE CHEZ POINT JARDIN !</h2><br><br>
+		<h5 class="qualite">La qualité à prix malin.</h5>
+		<h5 class="pl">Paul et Laurentine vous accueillent dans votre nouveau<br/> point de vente de matériaux pour l’aménagement du jardin.</h5>
+	</div>
+
+	<br><br>
+
+	<ul class="flex-container">
+		<li class="flex-item"><img src="imgs/n1.png" onmouseover="this.src='imgs/n1o.png'" onmouseout="this.src='imgs/n1.png'" /><br>Achetez la quantité dont vous avez besoin.</li>
+		
+		<li class="flex-item"><img src="imgs/n2.png" onmouseover="this.src='imgs/n2o.png'" onmouseout="this.src='imgs/n2.png'" /><br>En panne d’inspiration?<br>Nous vous conseillons et dessinons votre jardin.</li>
+		
+		
+		<li class="flex-item"><img src="imgs/n3.png" onmouseover="this.src='imgs/n3o.png'" onmouseout="this.src='imgs/n3.png'" /><br>Embellisez votre jardin avec notre gamme de paillage variés.</li>
+		
+		<li class="flex-item"><img src="imgs/n4.png" onmouseover="this.src='imgs/n4o.png'" onmouseout="this.src='imgs/n4.png'" /><br>Pas de véhicule adapté?<br>Point jardin vous livre chez vous, petite ou grande quantité.</li>
+	</ul>
+</div>
+	  
+
+<div class="section1">  
+	<center>
+		<div class="row">
+			<div class="col m6 s12">
+				<img class="circle" src="imgs/fondatrice.png" alt="Fondatrice."/>
+				<h5 class="white-text">Laurentine TIERCELIN :</h5>
+				<p class="grey-text text-lighten-4">26 ans.</p>
+				<p class="grey-text text-lighten-4">Conceptrice paysagiste.</p>
+				<p class="grey-text text-lighten-4">Licence d'aménagements paysagers d'Angers.</p>
+				<p class="grey-text text-lighten-4">4 années de dessinatrice paysagiste en bureau d'études parisien.</p>
+			</div>
+			<div class="col m6 s12">
+				<img class="circle" src="imgs/fondateur.png" alt="Fondateur."/>
+				<h5 class="white-text">Paul LECAT :</h5>
+				<p class="grey-text text-lighten-4">25 ans.</p>
+				<p class="grey-text text-lighten-4">Ingénieur paysagiste.</p>
+				<p class="grey-text text-lighten-4">Ecole d'ingénieur du paysage ITIAPE de Lille.</p>
+				<p class="grey-text text-lighten-4">4 années de conducteur de travaux en entreprises de paysage.</p>
+			</div>
+		</div>
+	</center>
+</div>
+<footer class="page-footer">
+	<div class="footer-copyright">
+		<div class="container">
+			© 2016 Copyright Point Jardin.
+			<a href="#top">
+				<img class="grey-text text-lighten-4 right" src="imgs/f.png" onmouseover="this.src='imgs/fo.png'" onmouseout="this.src='imgs/f.png'" />
+			</a>
+			<a href="https://www.youtube.com/watch?v=szGkJgvt2-0"">
+				<img class="green-text text-darken-1 right"  src="imgs/youtube_image.png" onmouseover="this.src='imgs/youtubeo.png'" onmouseout="this.src='imgs/youtube_image.png'" />
+			</a>
+			<a href="#">
+				<img class="grey-text text-lighten-4 right" src=""/>
+			</a>
+			<a href="https://www.facebook.com/pages/Point-Jardin/861988743863661"">
+				<img class="grey-text text-lighten-4 right" src="imgs/facebook-image.png" onmouseover="this.src='imgs/facebooko.png'" onmouseout="this.src='imgs/facebook-image.png'" />
+			</a>
+		</div>
+	</div>
+</footer>  
+
+
+
+
+
+  
+  
+<!-- jQuery is required by Materialize to function. -->
+<script>
+$(document).ready(function(){
+    $('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+  });
+</script>
+
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script><!-- Load JS Needed-->
+<script src="js/materialize.min.js"></script>
+<script type="text/javascript"></script>
+
+</body>
+</html>
